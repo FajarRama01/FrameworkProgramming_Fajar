99843f8d1900c35e9579053876f00ff3f4d6fff0
FajarRama01 <ramadlan2019@gmail.com>
2025-11-13 11:17:09 +0700

    P1

---
+++ data_kelurahan/warga/urls.py
@@ -0,0 +1,8 @@
+# warga/urls.py
+from django.urls import path
+from .views import WargaListView, WargaDetailView
+
+urlpatterns = [
+    path('', WargaListView.as_view(), name='warga-list'),
+    path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail')
+]
